c5ffdf37533afa8b92ee30e2e0b1ac7ac014015e
Tony Eder <tonyeder11@gmail.com>
2024-04-29 21:02:24 -0700

    needed assistance with this one

--- server/app.py
+++ server/app.py
@@ -22,13 +22,43 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    response = make_response(
+        articles, 
+        200
+        )
 
-    pass
+    return response
 
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
+    breakpoint()
+    if 'page_views' not in session:
+        session['page_views'] = 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        if article is None:
+            return jsonify({'message': 'Article not found'}), 404
+
+        article_to_dict = article.to_dict()
+        response = make_response(jsonify(article_to_dict), 200)
+        return response
+    
+    else:
+        response_body = {
+            "message" : "Maximum pageview limit reached"
+        }
+        
+        return make_response(
+            response_body,
+            401
+        )
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
