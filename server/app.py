3cf6897e9623e6cfcc2243826554de6721a313b0
Tony Eder <tonyeder11@gmail.com>
2024-04-30 08:32:23 -0700

    updated with flask-restful to test

--- server/app.py
+++ server/app.py
@@ -2,10 +2,12 @@
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
+from flask_restful import Resource, Api
 
 from models import db, Article, User
 
 app = Flask(__name__)
+api = Api(app)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -30,32 +32,58 @@ def index_articles():
 
     return response
 
-@app.route('/articles/<int:id>', methods=['GET'])
-def show_article(id):
-    if 'page_views' not in session:
-        session['page_views'] = 0
-    session['page_views'] += 1
+# @app.route('/articles/<int:id>', methods=['GET'])
+# def show_article(id):
+#     if 'page_views' not in session:
+#         session['page_views'] = 0
+#     session['page_views'] += 1
 
-    if session['page_views'] <= 3:
-        article = Article.query.filter_by(id=id).first()
-        if article is None:
-            return jsonify({'message': 'Article not found'}), 404
+#     if session['page_views'] <= 3:
+#         article = Article.query.filter_by(id=id).first()
+#         if article is None:
+#             return jsonify({'message': 'Article not found'}), 404
 
-        article_to_dict = article.to_dict()
-        response = make_response(jsonify(article_to_dict), 200)
-        return response
+#         article_to_dict = article.to_dict()
+#         response = make_response(jsonify(article_to_dict), 200)
+#         return response
     
-    else:
-        response_body = {
-            "message" : "Maximum pageview limit reached"
-        }
+#     else:
+#         response_body = {
+#             "message" : "Maximum pageview limit reached"
+#         }
         
-        return make_response(
-            response_body,
-            401
-        )
+#         return make_response(
+#             response_body,
+#             401
+#         )
 
+class Show_Article(Resource):
+    def get(self, id):
+        if 'page_views' not in session:
+            session['page_views'] = 0
+        session['page_views'] += 1
 
+        if session['page_views'] <= 3:
+            article = Article.query.filter_by(id = id).first()
+            article_to_dict = article.to_dict()
+            response = make_response(
+                jsonify(article_to_dict),
+                200
+            )
+            return response
+
+        else: 
+            response_body = {
+                "message" : "Maximum pageview limit reached"
+            }
+
+            return make_response(
+                response_body,
+                401
+            )
+        
+
+api.add_resource(Show_Article, '/articles/<int:id>')
 
 
 
